cb39f79849aa8272ff80415de6cf618d138d1f61
Haşim Yiğitgülsün <hasimyigitt@gmail.com>
2024-07-22 16:41:44 +0300

    Factory Design

---
+++ Day-5/index.ts
@@ -0,0 +1,65 @@
+import { describe } from "node:test";
+
+//models/user.ts
+export interface User {
+  id: string;
+  name: string;
+  lastName: string;
+  phone: string;
+  email: string;
+  age: number;
+}
+
+// Before
+  const USER_MOCK: User = {
+    id: "123-asd",
+    name: "Tom",
+    lastName: "Riddle",
+    phone: "+90 555 55 55",
+    email: "tom_riddle@gmail.com",
+    age: 19,
+  };
+
+  const USERS_MOCK: User[] = [
+    USER_MOCK,
+    { ...USER_MOCK, id: "1" },
+    { ...USER_MOCK, id: "1" },
+  ];
+
+  //Test file
+  //users.service.test.ts
+  describe("users service", () => {
+    it("allow fetching users", async () => {
+      const response = { data: USER_MOCK, status: 200 };
+    });
+  });
+
+
+// After
+// users.factory.ts
+  const usersFactory = (data: Partial<User> = {}): User => {
+    //Object.Freeze blocks direct modifications.
+    return Object.freeze({
+      id: "123-asd",
+      name: "Tom",
+      lastName: "Riddle",
+      phone: "+90 555 55 55",
+      email: "tom_riddle@gmail.com",
+      age: 19,
+      ...data, // Adding data allows overriding default properties
+    }) satisfies User;
+  };
+
+
+
+  //Test file
+  //users.service.test.ts
+  describe("users service", () => {
+    it("allow fetching users", async () => {
+      const response = { data: usersFactory(), status: 200 };
+
+      //You may override properties like this:
+      const customUser = usersFactory({ id: "my-other-id" });
+    });
+  });
+
